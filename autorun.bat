f20566150bca6e6ff64f988ef2e5aec20472df63
Yufei Pan <demonte@zopyr.us>
2022-01-29 02:30:02 -0800

    Create autorun.bat

---
+++ autorun.bat
@@ -0,0 +1,3 @@
+start "" https://laptop-updates.brave.com/latest/winx64
+start "" https://www.intel.com/content/www/us/en/download-center/home.html
+wmic useraccount where name='currentname' rename newname
